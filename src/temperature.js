dbd265ebe46adb88079298940b9c835c1ca1ca60
Bruno Bigotto <brunobigotto@hotmail.com>
2020-02-19 21:59:29 -0400

    Fatoração de trechos semelhantes

--- src/temperature.js
+++ src/temperature.js
@@ -3,20 +3,16 @@ const request = require('request')
 const getTemperatureCity = (name, callback) => {
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&appid=be69748fc857baf4586b9c7ac66566c6`
 
-    request({url, json: true}, (error, {body}) => {
-        if(error) {
-            callback('Unable to connect to weather service', undefined)
-        } else if( body.error){
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.main.temp)
-        }
-    })
+    return getResult(url, callback)
 }
 // [-20.44, -54.65]
 const getTemperatureCoord = (lat, lon, callback) => {
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=be69748fc857baf4586b9c7ac66566c6`
 
+    return getResult(url, callback)
+}
+
+const getResult = (url, callback) => {
     request({url, json: true}, (error, {body}) => {
         if(error) {
             callback('Unable to connect to weather service', undefined)
