39f984a576be11d55ac885d5ec5094c2767574e5
Bruno Bigotto <brunobigotto@hotmail.com>
2020-02-23 12:27:55 -0400

    Retorno dos títulos das músicas da playlist sugerida acordo com o clima

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { getTemperatureCity, getTemperatureCoord } = require('./temperature')
+const { getGenre, getTracks } = require('./playlist')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,22 +13,29 @@ app.get('/city', (req, res) => {
     const city = req.query.name
     getTemperatureCity(city, (error, temp) => {
         if (error) {
-            return res.send({ error })
+            return res.send(error)
         }
-        res.send({
-            temp
+        const genre = getGenre(temp)
+        getTracks(genre, (error, tracks) => {
+            if (error) {
+                return res.send(error)
+            }
+            res.send(tracks)
         })
-    })
+    })     
 })
 
 app.get('/coord', (req, res) => {
     const { lat, lon } = req.query
     getTemperatureCoord(lat, lon, (error, temp) => {
         if (error) {
-            return res.send({ error })
+            return res.send(error)
         }
-        res.send({
-            temp
+        const genre = getGenre(temp)
+        getTracks(genre, (error, tracks) => {
+            if(error)
+                return res.send(error)
+            res.send(tracks)
         })
     })
 })
